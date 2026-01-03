d5c2f359cac845d26997d7fc90f6e2102ac6e027
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-01-03 11:39:02 -0800

    add SetListPerformanceTest

--- src/com/learn/sets/homework/TestTreeSetWithComparator.java
+++ src/com/learn/sets/homework/TestTreeSetWithComparator.java
@@ -1,22 +1,22 @@
-package com.learn.sets.homework;
-
-import java.awt.*;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
-
-public class TestTreeSetWithComparator {
-    public static void main(String[] args) {
-
-        Set<Rectangle> set=
-                new TreeSet<>((Comparator) new GeometricObjectComparator());
-        set.add(new Rectangle(4, 5));
-        set.add(new Circle(40));
-        set.add(new Circle(40));
-        set.add(new Rectangle(4, 1));
-
-        System.out.println("A sorted set of geometric objects");
-        for (GeometricObject element: set)
-            System.out.println("area = " + element.getArea());
-    }
-}
+//package com.learn.sets.homework;
+//
+//import java.awt.*;
+//import java.util.Comparator;
+//import java.util.Set;
+//import java.util.TreeSet;
+//
+//public class TestTreeSetWithComparator {
+//    public static void main(String[] args) {
+//
+//        Set<Rectangle> set=
+//                new TreeSet<>((Comparator) new GeometricObjectComparator());
+//        set.add(new Rectangle(4, 5));
+//        set.add(new Circle(40));
+//        set.add(new Circle(40));
+//        set.add(new Rectangle(4, 1));
+//
+//        System.out.println("A sorted set of geometric objects");
+//        for (GeometricObject element: set)
+//            System.out.println("area = " + element.getArea());
+//    }
+//}
